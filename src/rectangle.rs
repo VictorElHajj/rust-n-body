da860334f388b73b43562e5fe76240fd1ec43255
MysticPing <hajjvictor@live.se>
2021-01-05 16:12:29 +0100

    Use borrowed vec in inside and make public pos and size

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -1,12 +1,12 @@
 use crate::vector::Vector2;
 pub struct Rectangle {
     /// Top left corner of rectangle
-    pos: Vector2,
-    size: f64,
+    pub pos: Vector2,
+    pub size: f64,
 }
 
 impl Rectangle {
-    fn inside(&self, p: Vector2) -> bool {
+    pub fn inside(&self, p: &Vector2) -> bool {
         let s = &self.pos;
         return p.x >= s.x && p.x <= (s.x + self.size) && p.y >= s.y && p.y <= (s.y + self.size);
     }
@@ -25,7 +25,7 @@ mod tests {
         let p1 = Vector2::new(1.0, 4.0);
         let p2 = Vector2::new(-6.0, 4.0);
 
-        assert!(r.inside(p1));
-        assert!(!r.inside(p2));
+        assert!(r.inside(&p1));
+        assert!(!r.inside(&p2));
     }
 }
