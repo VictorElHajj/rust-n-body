c66004eb33e2d0a130619bc354dd2136a9f093c8
MysticPing <hajjvictor@live.se>
2021-01-05 16:50:33 +0100

    Rectangle split helpers

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -1,4 +1,5 @@
 use crate::vector::Vector2;
+#[derive(PartialEq, Debug)]
 pub struct Rectangle {
     /// Top left corner of rectangle
     pub pos: Vector2,
@@ -9,6 +10,34 @@ impl Rectangle {
     pub fn inside(&self, p: &Vector2) -> bool {
         let s = &self.pos;
         return p.x >= s.x && p.x <= (s.x + self.size) && p.y >= s.y && p.y <= (s.y + self.size);
+    }
+
+    pub fn ne(&self) -> Rectangle {
+        Rectangle {
+            pos: Vector2::new(self.pos.x + self.size / 2.0, self.pos.y),
+            size: self.size / 2.0,
+        }
+    }
+
+    pub fn se(&self) -> Rectangle {
+        Rectangle {
+            pos: Vector2::new(self.pos.x + self.size / 2.0, self.pos.y + self.size / 2.0),
+            size: self.size / 2.0,
+        }
+    }
+
+    pub fn sw(&self) -> Rectangle {
+        Rectangle {
+            pos: Vector2::new(self.pos.x, self.pos.y + self.size / 2.0),
+            size: self.size / 2.0,
+        }
+    }
+
+    pub fn nw(&self) -> Rectangle {
+        Rectangle {
+            pos: Vector2::new(self.pos.x, self.pos.y),
+            size: self.size / 2.0,
+        }
     }
 }
 
@@ -27,5 +56,33 @@ mod tests {
 
         assert!(r.inside(&p1));
         assert!(!r.inside(&p2));
+    }
+
+    #[test]
+    fn test_square_split() {
+        let r = Rectangle {
+            pos: Vector2::new(-5.0, -5.0),
+            size: 10.0,
+        };
+        let ne = Rectangle {
+            pos: Vector2::new(0.0, -5.0),
+            size: 5.0,
+        };
+        let se = Rectangle {
+            pos: Vector2::new(0.0, 0.0),
+            size: 5.0,
+        };
+        let sw = Rectangle {
+            pos: Vector2::new(-5.0, 0.0),
+            size: 5.0,
+        };
+        let nw = Rectangle {
+            pos: Vector2::new(-5.0, -5.0),
+            size: 5.0,
+        };
+        assert_eq!(ne, r.ne());
+        assert_eq!(se, r.se());
+        assert_eq!(sw, r.sw());
+        assert_eq!(nw, r.nw());
     }
 }
