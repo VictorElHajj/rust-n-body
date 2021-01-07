3cb888eea42a27b0c6be4d91312e0f4fa0b551c1
MysticPing <hajjvictor@live.se>
2021-01-07 22:20:05 +0100

    Implemebt vector/scalara math and helpers

--- src/vector.rs
+++ src/vector.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Div, Mul, Sub};
+
 #[derive(PartialEq, Debug, Copy, Clone)]
 pub struct Vector2 {
     pub x: f64,
@@ -5,11 +7,67 @@ pub struct Vector2 {
 }
 
 impl Vector2 {
-    pub fn new(x: f64, y: f64) -> Vector2 {
+    pub fn new(x: f64, y: f64) -> Self {
         Vector2 { x, y }
     }
 
     pub fn zero() -> Vector2 {
         Vector2 { x: 0.0, y: 0.0 }
+    }
+
+    pub fn distance(&self, other: Self) -> f64 {
+        ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+    }
+
+    pub fn distance_2(&self, other: Self) -> f64 {
+        (self.x - other.x).powi(2) + (self.y - other.y).powi(2)
+    }
+
+    pub fn normal_vector_between(&self, other: Self) -> Self {
+        (other - *self) / other.distance(*self)
+    }
+}
+
+impl Add for Vector2 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Vector2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Vector2 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Vector2 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl Mul<f64> for Vector2 {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self::Output {
+        Vector2 {
+            x: self.x * other,
+            y: self.y * other,
+        }
+    }
+}
+
+impl Div<f64> for Vector2 {
+    type Output = Self;
+
+    fn div(self, other: f64) -> Self::Output {
+        Vector2 {
+            x: self.x / other,
+            y: self.y / other,
+        }
     }
 }
