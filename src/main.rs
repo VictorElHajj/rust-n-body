040e93af9fed233d34ea48823123b688c3594d1c
MysticPing <hajjvictor@live.se>
2021-01-07 19:35:05 +0100

    Debug, visualize and fix with ggez

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use ggez::event::{self, EventHandler};
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+use n_body::{body::Body, quadtree::QuadTree, rectangle::Rectangle, vector::Vector2};
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let mut qt = QuadTree::new(Rectangle {
+        pos: Vector2::new(0.0, 0.0),
+        size: 500.0,
+    });
+    for _ in 0..1000 {
+        let b = Body {
+            pos: Vector2::new(rng.gen_range(0.0..800.0), rng.gen_range(0.0..800.0)),
+            vel: Vector2::zero(),
+            mass: rng.gen_range(1.0..100.0),
+        };
+        qt.insert(b).ok();
+    }
+
+    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+        .build()
+        .expect("aieee, could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let mut my_game = MyGame::new(&mut ctx, qt);
+
+    // Run!
+    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e),
+    }
+}
+
+struct MyGame {
+    qt: QuadTree,
+    // Your state here...
+}
+
+impl MyGame {
+    pub fn new(_ctx: &mut Context, qt: QuadTree) -> MyGame {
+        // Load/create resources such as images here.
+        MyGame {
+            qt
+        }
+    }
+}
+
+impl EventHandler for MyGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::BLACK);
+        self.qt.draw(ctx)?;
+        graphics::present(ctx)?;
+
+        Ok(())
+    }
 }
--- src/quadtree.rs
+++ src/quadtree.rs
@@ -1,3 +1,5 @@
+use ggez::{Context, GameResult, graphics::{self}, mint::Point2};
+
 use crate::{body::Body, physics_helper::*, rectangle::Rectangle, vector::Vector2};
 
 /// A quadtree with a bucket size of one
@@ -37,16 +39,8 @@ impl QuadTree {
     fn subdivide(&mut self) {
         match self {
             QuadTree::Leaf(Leaf { boundary, body }) => {
-                let mut qt = QuadTree::Root(Root {
-                    boundary: *boundary,
-                    center_of_mass: Vector2::new(0.0, 0.0),
-                    mass: 0.0,
-                    ne: None,
-                    se: None,
-                    sw: None,
-                    nw: None,
-                });
-                qt.insert(*body);
+                let mut qt = QuadTree::new(*boundary);
+                qt.insert(*body).ok();
                 *self = qt;
             }
             _ => (),
@@ -56,68 +50,138 @@ impl QuadTree {
     pub fn insert(&mut self, b1: Body) -> Result<(), &'static str> {
         match self {
             // Occupied leaf, split into root
-            QuadTree::Leaf(Leaf {
-                boundary,
-                body: _,
-            }) => {
-                if boundary.contains(&b1.pos) {
+            QuadTree::Leaf(leaf) => {
+                if leaf.boundary.contains(&b1.pos) {
                     self.subdivide();
                     return self.insert(b1);
                 } else {
                     return Err("Inserted body is outside boundary");
                 }
             }
-            // Empty leaf, just enter the body
-            QuadTree::Leaf(leaf) => {
-                if leaf.boundary.contains(&b1.pos) {
-                    leaf.body = b1;
-                    return Ok(());
-                } else {
-                    return Err("Inserted body is outside boundary");
-                }
-            }
             QuadTree::Root(root) => {
                 if root.boundary.north_east().contains(&b1.pos) {
-                    let qt = QuadTree::Leaf(Leaf {
-                        boundary: root.boundary.north_east(),
-                        body: b1,
-                    });
-                    root.ne = Some(Box::new(qt));
-                    root.center_of_mass = calc_com(b1.pos, b1.mass, root.center_of_mass, root.mass);
-                    root.mass += b1.mass;
-                    Ok(())
+                    match &mut root.ne {
+                        None => {
+                            let qt = QuadTree::Leaf(Leaf {
+                                boundary: root.boundary.north_east(),
+                                body: b1,
+                            });
+                            root.ne = Some(Box::new(qt));
+                            root.center_of_mass = calc_com(b1.pos, b1.mass, root.center_of_mass, root.mass);
+                            root.mass += b1.mass;
+                            Ok(())
+                        } 
+                        Some(qt) => {
+                           qt.insert(b1)
+                        }
+                    }
                 } else if root.boundary.south_east().contains(&b1.pos) {
-                    let qt = QuadTree::Leaf(Leaf {
-                        boundary: root.boundary.south_east(),
-                        body: b1,
-                    });
-                    root.se = Some(Box::new(qt));
-                    root.center_of_mass = calc_com(b1.pos, b1.mass, root.center_of_mass, root.mass);
-                    root.mass += b1.mass;
-                    Ok(())
+                    match &mut root.se {
+                        None => {
+                            let qt = QuadTree::Leaf(Leaf {
+                                boundary: root.boundary.south_east(),
+                                body: b1,
+                            });
+                            root.se = Some(Box::new(qt));
+                            root.center_of_mass = calc_com(b1.pos, b1.mass, root.center_of_mass, root.mass);
+                            root.mass += b1.mass;
+                            Ok(())
+                        } 
+                        Some(qt) => {
+                           qt.insert(b1)
+                        }
+                    }
                 } else if root.boundary.south_west().contains(&b1.pos) {
-                    let qt = QuadTree::Leaf(Leaf {
-                        boundary: root.boundary.south_west(),
-                        body: b1,
-                    });
-                    root.sw = Some(Box::new(qt));
-                    root.center_of_mass = calc_com(b1.pos, b1.mass, root.center_of_mass, root.mass);
-                    root.mass += b1.mass;
-                    Ok(())
+                    match &mut root.sw {
+                        None => {
+                            let qt = QuadTree::Leaf(Leaf {
+                                boundary: root.boundary.south_west(),
+                                body: b1,
+                            });
+                            root.sw = Some(Box::new(qt));
+                            root.center_of_mass = calc_com(b1.pos, b1.mass, root.center_of_mass, root.mass);
+                            root.mass += b1.mass;
+                            Ok(())
+                        } 
+                        Some(qt) => {
+                           qt.insert(b1)
+                        }
+                    }
                 } else if root.boundary.north_west().contains(&b1.pos) {
-                    let qt = QuadTree::Leaf(Leaf {
-                        boundary: root.boundary.north_west(),
-                        body: b1,
-                    });
-                    root.nw = Some(Box::new(qt));
-                    root.center_of_mass = calc_com(b1.pos, b1.mass, root.center_of_mass, root.mass);
-                    root.mass += b1.mass;
-                    Ok(())
+                    match &mut root.nw {
+                        None => {
+                            let qt = QuadTree::Leaf(Leaf {
+                                boundary: root.boundary.north_west(),
+                                body: b1,
+                            });
+                            root.nw = Some(Box::new(qt));
+                            root.center_of_mass = calc_com(b1.pos, b1.mass, root.center_of_mass, root.mass);
+                            root.mass += b1.mass;
+                            Ok(())
+                        } 
+                        Some(qt) => {
+                           qt.insert(b1)
+                        }
+                    }
                 } else {
                     return Err("Inserted body is outside boundary");
                 }
             }
         }
+    }
+
+    pub fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        match self {
+            QuadTree::Leaf(leaf) => {
+                // Draw boundary
+                let bounds = graphics::Rect::new(leaf.boundary.pos.x as f32, leaf.boundary.pos.y as f32, leaf.boundary.size as f32, leaf.boundary.size as f32);
+                let rectangle = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(1.0), bounds, graphics::WHITE)?;
+                graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
+                // Draw body
+                let circle = graphics::Mesh::new_circle(ctx, graphics::DrawMode::fill(), Point2 {x: leaf.body.pos.x as f32, y: leaf.body.pos.y as f32}, 2.0, 1.0, graphics::WHITE)?;
+                graphics::draw(ctx, &circle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
+                Ok(())
+            } 
+            QuadTree::Root(root) => {
+                match &root.ne {
+                    None => {
+                        let r = root.boundary.north_east();
+                        let bounds = graphics::Rect::new(r.pos.x as f32, r.pos.y as f32, r.size as f32, r.size as f32);
+                        let rectangle = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(1.0), bounds, graphics::WHITE)?;
+                        graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))
+                    },
+                    Some(qt ) => qt.draw(ctx),
+                }.ok(); 
+                match &root.se {
+                    None => {
+                        let r = root.boundary.south_east();
+                        let bounds = graphics::Rect::new(r.pos.x as f32, r.pos.y as f32, r.size as f32, r.size as f32);
+                        let rectangle = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(1.0), bounds, graphics::WHITE)?;
+                        graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))
+                    },
+                    Some(qt ) => qt.draw(ctx),
+                }.ok(); 
+                match &root.sw {
+                    None => {
+                        let r = root.boundary.south_west();
+                        let bounds = graphics::Rect::new(r.pos.x as f32, r.pos.y as f32, r.size as f32, r.size as f32);
+                        let rectangle = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(1.0), bounds, graphics::WHITE)?;
+                        graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))
+                    },
+                    Some(qt ) => qt.draw(ctx),
+                }.ok(); 
+                match &root.nw {
+                    None => {
+                        let r = root.boundary.north_west();
+                        let bounds = graphics::Rect::new(r.pos.x as f32, r.pos.y as f32, r.size as f32, r.size as f32);
+                        let rectangle = graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(1.0), bounds, graphics::WHITE)?;
+                        graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))
+                    },
+                    Some(qt ) => qt.draw(ctx),
+                }.ok(); 
+                Ok(())
+            } 
+      }
     }
 }
 
