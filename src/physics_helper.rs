d722f9fb0829b4c057d89ee7cb340f0e71982c5a
MysticPing <hajjvictor@live.se>
2021-01-08 21:06:55 +0100

    Gravity helpers

--- src/physics_helper.rs
+++ src/physics_helper.rs
@@ -1,4 +1,4 @@
-use crate::vector::Vector2;
+use crate::{body::Body, vector::Vector2};
 
 /// Calculate center of mass
 #[inline]
@@ -7,4 +7,13 @@ pub fn calc_com(pos1: Vector2, mass1: f64, pos2: Vector2, mass2: f64) -> Vector2
         (pos1.x * mass1 + mass2 * pos2.x) / (mass1 + mass2),
         (pos1.y * mass1 + mass2 * pos2.y) / (mass1 + mass2),
     )
+}
+
+/// Not G, which is applied last as optimization
+pub fn calc_pull(b1: &Body, b2: &Body) -> Vector2 {
+    (b2.pos - b1.pos) / (b1.pos.distance(b2.pos)).powi(3) * (b1.mass * b2.mass)
+}
+
+pub fn calc_pull_com(b1: &Body, b2_pos: Vector2, b2_mass: f64) -> Vector2 {
+    (b2_pos - b1.pos) / (b1.pos.distance(b2_pos)).powi(3) * (b1.mass * b2_mass)
 }
